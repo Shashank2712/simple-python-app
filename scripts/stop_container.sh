47b4386ed26fec62bc3d7fc57bdd634ca4a53b7d
Shashank2712 <srk2712@gmail.com>
2023-08-07 17:31:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid = `docker ps | awk -F " " '{print $1}'`
-sudo docker rm -f $containerid
+# Check if a container is running
+if [[ $(docker ps -q) ]]; then
+  # Retrieve the container ID of the running container
+  containerid=$(docker ps -q | head -n1)
+
+  # Stop the running container
+  docker stop "$containerid"
+  echo "Container with ID $containerid stopped successfully."
+else
+  echo "No running container found. Proceeding without stopping any container."
+fi
+
