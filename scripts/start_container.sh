85009db2fadafddda381d25ed3b90f2765eeb43b
Shashank2712 <srk2712@gmail.com>
2023-08-07 14:59:15 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull koli2712/sample-python-flask-service 
+sudo docker pull koli2712/sample-python-flask-service 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 koli2712/sample-python-flask-service
+sudo docker run -d -p 5000:5000 koli2712/sample-python-flask-service
