14c98aee2da2e16dbe2995a8bf7ae8ea494fdca0
Shashank2712 <srk2712@gmail.com>
2023-08-08 10:47:09 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull koli2712/simple-python-flask-app:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker pull koli2712/simple-python-flask-app:latest
+docker run -d -p 5000:5000 koli2712/simple-python-flask-app:latest
